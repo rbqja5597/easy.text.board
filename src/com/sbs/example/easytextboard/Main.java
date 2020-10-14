bb6a6dab66b4835af8bc02bca3c75d85c1f0cf2b
rbqja5597 <rbqja5597@naver.com>
2020-10-14 19:58:52 +0900

    게시물 상세보기 , 클래스 생성

--- src/com/sbs/example/easytextboard/Main.java
+++ src/com/sbs/example/easytextboard/Main.java
@@ -8,7 +8,7 @@ public class Main { // 클래스
 
 		// 1번 게시물 저장소
 		Article article1 = new Article();
-
+		
 		// 2번 게시물 저장소
 		Article article2 = new Article();
 
@@ -16,26 +16,27 @@ public class Main { // 클래스
 
 		while (true) {
 
-			System.out.printf("명령어1) ");
+			System.out.printf("명령어) ");
 			String command = scanner.nextLine();
 
 			if (command.equals("article add")) {
 				System.out.println("== 게시물 등록 ==");
 
-				int id = num + 1;
+				int id = num + 1; // num의 값은 0 , num + 1 의 값은 id , 1 = id
+									// num의 값은 1 , num + 1 의 값은 id , 2 = id
 
 				System.out.printf("제목: ");
 				String title = scanner.nextLine();
 				System.out.printf("내용: ");
 				String body = scanner.nextLine();
 
-				// System.out.printf("번호: %d\n", id);
-				// System.out.printf("제목: %s\n", title);
-				// System.out.printf("내용: %s\n", body);
+				System.out.printf("번호: %d\n", id);
+				System.out.printf("제목: %s\n", title);
+				System.out.printf("내용: %s\n", body);
 
-				// System.out.printf("%d번 게시물이 생성되었습니다.\n", id);
+				System.out.printf("%d번 게시물이 생성되었습니다.\n", id);
 
-				num = id;
+				num = id; // 위의 문장들을 실행 후 id와 num의 값은 같아짐. id = 1 , num = 1
 
 				if (num == 1) {
 					article1.id = id;
@@ -49,7 +50,6 @@ public class Main { // 클래스
 
 			} else if (command.equals("article list")) {
 				System.out.println("== 게시물 리스트 ==");
-
 				if (num == 0) {
 					System.out.println("게시물이 없습니다.");
 					continue;
@@ -65,38 +65,29 @@ public class Main { // 클래스
 
 			} else if (command.startsWith("article detail ")) {
 
-				int inputedID = Integer.parseInt(command.split(" ")[2]); // 두번째 공백의 값 = inpuedId
-				System.out.println("== 게시물 상세 ==");
+				int ID = Integer.parseInt(command.split(" ")[2]);
+				System.out.println("== 게시물 상세 == ");
 
-				if (inputedID == 1) { // 공백의 값이 1일때
-					if (article1.id == 0) {
-						System.out.printf("%d번 게시물은 존재하지 않습니다.\n", inputedID);
-						continue;
-					}
-
+				if (num == 0) {
+					System.out.printf("%d번 게시물이 없습니다\n", ID);
+					
+				} else if (ID == 1) {
 					System.out.printf("번호: %d\n", article1.id);
 					System.out.printf("제목: %s\n", article1.title);
 					System.out.printf("내용: %s\n", article1.body);
-				} else if (inputedID == 2) { // 공백의 값이 2일때
-					
-					if (article2.id == 0) {
-						System.out.printf("%d번 게시물은 존재하지 않습니다.\n", inputedID);
-						continue;
-					}
-					
+				} else if (ID == 2) {
 					System.out.printf("번호: %d\n", article2.id);
 					System.out.printf("제목: %s\n", article2.title);
 					System.out.printf("내용: %s\n", article2.body);
-				} else {
-					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", inputedID);
 				}
 
-			} else if (command.equals("article exit")) {
+			} else if (command.equals("system exit")) {
 				System.out.println("== 프로그램 종료 ==");
 				break;
 			} else {
 				System.out.println("== 존재하지 않는 명령어 ==");
 			}
+
 		}
 		scanner.close();
 	}
