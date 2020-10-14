5a0641e5c5eae122ff5a1af46ff794f47d5d7456
rbqja5597 <rbqja5597@naver.com>
2020-10-14 12:54:54 +0900

    클래스 생성

--- src/com/sbs/example/easytextboard/Main.java
+++ src/com/sbs/example/easytextboard/Main.java
@@ -2,18 +2,21 @@ package com.sbs.example.easytextboard; //폴더명은 소문자로 , 해결책 c
 
 import java.util.Scanner;
 
+class Article {
+	public int id;
+	public String title;
+	public String body;
+}
+
 public class Main { // 클래스
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-			
-		int article1_id = 0;
-		String article1_title = "";
-		String article1_body = "";
+		// 1번 게시물 저장소
+		Article article1 = new Article();
 
-		int article2_id = 0;
-		String article2_title = "";
-		String article2_body = "";
+		// 2번 게시물 저장소
+		Article article2 = new Article();
 
 		int num = 0;
 
@@ -41,13 +44,13 @@ public class Main { // 클래스
 				num = id;
 
 				if (num == 1) {
-					article1_id = id;
-					article1_title = title;
-					article1_body = body;
+					article1.id = id;
+					article1.title = title;
+					article1.body = body;
 				} else if (num == 2) {
-					article2_id = id;
-					article2_title = title;
-					article2_body = body;
+					article2.id = id;
+					article2.title = title;
+					article2.body = body;
 				}
 
 			} else if (command.equals("article list")) {
@@ -60,29 +63,29 @@ public class Main { // 클래스
 				System.out.println("번호 / 제목");
 
 				if (num >= 1) {
-					System.out.printf("%d / %s\n", article1_id, article1_title);
+					System.out.printf("%d / %s\n", article1.id, article1.title);
 				}
 				if (num >= 2) {
-					System.out.printf("%d / %s\n", article2_id, article2_title);
+					System.out.printf("%d / %s\n", article2.id, article2.title);
 				}
 
 			} else if (command.startsWith("article detail ")) {
 				
-				int inpuedID = Integer.parseInt(command.split(" ")[2]);
+				int inpuedID = Integer.parseInt(command.split(" ")[2]); // 두번째 공백의 값 = inpuedId 
 				System.out.println("== 게시물 상세 ==");
 				
-				if (inpuedID == 1 ) {
-					System.out.printf("번호: %d\n", article1_id);
-					System.out.printf("제목: %s\n", article1_title);
-					System.out.printf("내용: %s\n", article1_body);					
+				if (inpuedID == 1 ) { // 공백의 값이 1일때
+					System.out.printf("번호: %d\n", article1.id);
+					System.out.printf("제목: %s\n", article1.title);
+					System.out.printf("내용: %s\n", article1.body);					
 				}
-				else if (inpuedID == 2 ) {
-					System.out.printf("번호: %d\n", article2_id);
-					System.out.printf("제목: %s\n", article2_title);
-					System.out.printf("내용: %s\n", article2_body);					
+				else if (inpuedID == 2 ) { // 공백의 값이 2일때
+					System.out.printf("번호: %d\n", article2.id);
+					System.out.printf("제목: %s\n", article2.title);
+					System.out.printf("내용: %s\n", article2.body);					
 				}
 				
-				if (article1_id == 0 ) {
+				else if (article1.id == 0 ) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다. \n", inpuedID);
 					continue;
 				}
