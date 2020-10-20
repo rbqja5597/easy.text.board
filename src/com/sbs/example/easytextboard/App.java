032752a27fe8330ab748529009725ca05d92f55d
rbqja5597 <rbqja5597@naver.com>
2020-10-20 13:50:50 +0900

    게시물 삭제

--- src/com/sbs/example/easytextboard/App.java
+++ src/com/sbs/example/easytextboard/App.java
@@ -4,28 +4,63 @@ import java.util.Scanner;
 
 public class App {
 
-	Article[] articles = new Article[100]; // 0 ~ 9
-	
+	Article[] articles = new Article[10];
+	int num = 0;
+	int articlesCount = 0;
+
 	Article getArticle(int id) {
-		if ( id >= articles.length ) { 
+		if (id < 1) {
 			return null;
 		}
-		//System.out.println(id);
+		if (id > num) {
+			return null;
+		}
 		return articles[id - 1];
+
+	}
+
+	public void remove(int index) {
+		//System.out.printf("== %d번 좌석 숫자 제거 ==\n", index);
+		for (int i = index + 1 ; i < articlesCount; i++) {
+			articles[i - 1] = articles[i];
+
+		}
+
+		articlesCount--;
+	}
+
+	public int getIndexOf(int num) {
+		for (int i = 0; i < articles.length; i++) {
+			if (num == articles[i].id) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+
+	public void removeByValue(int num) {
+		int index = getIndexOf(num);
+		if (index != -1) {
+			remove(index);
+		}
+		
 		
 	}
-	
 
 	public void run() {
-		
-		for (int i=0; i < articles.length; i++) {
+
+		for (int i = 0; i < articles.length; i++) {
 			articles[i] = new Article();
 		}
-			
+		// for (int i=0; i < articles.length; i++) {
+		// articles[i].id = i + 1;
+		// System.out.printf("articles[%d].id : %d\n", i, articles[i].id);
+		// }
+
 		Scanner sc = new Scanner(System.in);
 
-		int num = 0; 
-		int maxArticlesCount = 3;
+		int maxArticlesCount = articles.length;
 
 		while (true) {
 
@@ -34,21 +69,21 @@ public class App {
 
 			if (command.equals("article add")) {
 				System.out.println("== 게시물 등록 ==");
-				if (num >= maxArticlesCount) {
+				if (articlesCount >= maxArticlesCount) {
 					System.out.println("더 이상 생성할 수 없습니다.");
 					continue;
 				}
 
-				int id = num + 1; // 0 + 1 
+				int id = num + 1; // 0 + 1
 
 				System.out.printf("제목: ");
 				String title = sc.nextLine();
-				System.out.printf("제목: ");
+				System.out.printf("내용: ");
 				String body = sc.nextLine();
 
 				System.out.printf("%d번 글이 생성되었습니다.\n", id);
 
-				num = id; // id의 값은 num과 같다  
+				num = id; // id의 값은 num과 같다
 
 				Article article = getArticle(num);
 
@@ -56,38 +91,52 @@ public class App {
 				article.title = title;
 				article.body = body;
 				
-				
+				articlesCount++;
 
 			} else if (command.equals("article list")) {
 				System.out.println("== 게시물 리스트 ==");
 
-				if (num == 0) {
+				if (articlesCount == 0) {
 					System.out.println("게시물이 존재하지 않습니다.");
 					continue;
 				}
 				System.out.println("번호 / 제목");
 
-				for (int i = 1; i <= num; i++) {
-					
+				for (int i = 1; i <= articlesCount; i++) {
+
 					Article article = getArticle(i);
 
 					System.out.printf("%d / %s\n", article.id, article.title);
 
 				}
+
+				
 			} else if (command.startsWith("article detail ")) {
 				int inputedId = Integer.parseInt(command.split(" ")[2]);
 				System.out.println("== 게시물 상세 ==");
+				
+				Article article = getArticle(inputedId);
 
-				if (num == 0 || num < inputedId) { //inputedId 가 num 보다 작으면
+				if (article == null|| num < inputedId) { // inputedId 가 num 보다 작으면
 					System.out.printf("%d번 글이 존재하지 않습니다.\n", inputedId);
 					continue;
 				}
 
-				Article article = getArticle(inputedId);
+				
 
 				System.out.printf("번호 : %d\n", article.id);
 				System.out.printf("제목 : %s\n", article.title);
 				System.out.printf("내용 : %s\n", article.body);
+
+			} else if (command.startsWith("article delete ")) {
+				int inp = Integer.parseInt(command.split(" ")[2]);
+				System.out.println("== 게시물 삭제 ==");
+
+				System.out.printf("%d번 게시물이 삭제되었습니다.\n", inp);
+				
+				int indexOf = getIndexOf(inp); 
+				System.out.printf("indexOf : %d\n", indexOf);
+				removeByValue(inp);
 
 			} else if (command.equals("system exit")) {
 				System.out.println("== 프로그램 종료 ==");
--- src/com/sbs/example/easytextboard/Main.java
+++ src/com/sbs/example/easytextboard/Main.java
@@ -1,5 +1,7 @@
 package com.sbs.example.easytextboard; //폴더명은 소문자로 , 해결책 ctrl + 2 
 
+
+
 public class Main { // 클래스
 	public static void main(String[] args) {
 		new App().run();
