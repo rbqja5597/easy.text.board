b38149211640398a6af6116a494e6070f4e5cbdb
rbqja5597 <rbqja5597@naver.com>
2020-10-21 11:27:04 +0900

    게시물 삭제

--- src/com/sbs/example/easytextboard/App.java
+++ src/com/sbs/example/easytextboard/App.java
@@ -4,25 +4,31 @@ import java.util.Scanner;
 
 public class App {
 
-	Article[] articles = new Article[10];
+	Article[] articles = new Article[3];
 	int num = 0;
 	int articlesCount = 0;
 
+	int articlesSize() {
+		return articlesCount;
+	}
+
 	Article getArticle(int id) {
-		if (id < 1) {
+		int index = getIndexOf(id);
+
+		if (index == -1) {
 			return null;
 		}
-		if (id > num) {
-			return null;
-		}
-		return articles[id - 1];
+		return articles[index];
 
 	}
 
-	public void remove(int index) {
-		//System.out.printf("== %d번 좌석 숫자 제거 ==\n", index);
-		for (int i = index ; i < articlesCount; i++) {
-			articles[i] = articles[i + 1];
+	public void remove(int id) {
+		int index = getIndexOf(id); 
+		if (index == -1) { 
+			return; 
+		}
+		for (int i = index + 1; i < articlesSize(); i++) {
+			articles[i - 1] = articles[i];
 
 		}
 
@@ -30,7 +36,7 @@ public class App {
 	}
 
 	public int getIndexOf(int num) {
-		for (int i = 0; i < articlesCount; i++) {
+		for (int i = 0; i < articlesSize(); i++) {
 			if (num == articles[i].id) {
 				return i;
 			}
@@ -38,28 +44,15 @@ public class App {
 		return -1;
 	}
 
-
-	public void removeByValue(int num) {
-		int index = getIndexOf(num); /// index = 1 일때 > 
-		if (index != -1) { // index의 값이 -1 과 같지 않을 경우
-			remove(index); // index 값을 remove
-		}
-
-	}
-
 	public void run() {
 
-		for (int i = 0; i < articles.length; i++) {
-			articles[i] = new Article();
-		}
-		// for (int i=0; i < articles.length; i++) {
-		// articles[i].id = i + 1;
-		// System.out.printf("articles[%d].id : %d\n", i, articles[i].id);
+		// for (int i = 0; i < articles.length; i++) {
+		// articles[i] = new Article();
 		// }
 
 		Scanner sc = new Scanner(System.in);
 
-		int maxArticlesCount = 3;
+		int maxArticlesCount = articles.length;
 
 		while (true) {
 
@@ -68,8 +61,8 @@ public class App {
 
 			if (command.equals("article add")) {
 				System.out.println("== 게시물 등록 ==");
-				if (articlesCount >= maxArticlesCount) {
-					System.out.println("더 이상 생성할 수 없습니다.");
+				if (articlesSize() >= maxArticlesCount) {
+					System.out.println("더 이상 추가할 수 없습니다.");
 					continue;
 				}
 
@@ -84,44 +77,43 @@ public class App {
 
 				num = id; // id의 값은 num과 같다
 
-				Article article = getArticle(num);
+				Article article = new Article();
+
+				articles[articlesCount] = article;
 
 				article.id = id;
 				article.title = title;
 				article.body = body;
-				
+
 				articlesCount++;
 
 			} else if (command.equals("article list")) {
 				System.out.println("== 게시물 리스트 ==");
 
-				if (articlesCount == 0) {
+				if (articlesSize() == 0) {
 					System.out.println("게시물이 존재하지 않습니다.");
 					continue;
 				}
 				System.out.println("번호 / 제목");
 
-				for (int i = 1; i <= articlesCount; i++) {
+				for (int i = 0; i < articlesSize(); i++) {
 
-					Article article = getArticle(i);
+					Article article = articles[i];
 
 					System.out.printf("%d / %s\n", article.id, article.title);
 
 				}
 
-				
 			} else if (command.startsWith("article detail ")) {
 				int inputedId = Integer.parseInt(command.split(" ")[2]);
 				System.out.println("== 게시물 상세 ==");
-				
+
 				Article article = getArticle(inputedId);
 
-				if (article == null|| num < inputedId) { // inputedId 가 num 보다 작으면
+				if (article == null || num < inputedId) { // inputedId 가 num 보다 작으면
 					System.out.printf("%d번 글이 존재하지 않습니다.\n", inputedId);
 					continue;
 				}
-
-				
 
 				System.out.printf("번호 : %d\n", article.id);
 				System.out.printf("제목 : %s\n", article.title);
@@ -131,11 +123,27 @@ public class App {
 				int inp = Integer.parseInt(command.split(" ")[2]);
 				System.out.println("== 게시물 삭제 ==");
 
-				System.out.printf("%d번 게시물이 삭제되었습니다.\n", inp);
+				Article article = getArticle(inp);
+
+				if (article == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", inp);
+					continue;
+				}
+
+				remove(inp);
 				
-				int indexOf = getIndexOf(inp); 
-				System.out.printf("indexOf : %d\n", indexOf);
-				removeByValue(inp);
+				System.out.printf("%d번 게시물이 삭제되었습니다.\n", inp);
+
+			} else if (command.startsWith("article modify ")) {
+				int inp = Integer.parseInt(command.split(" ")[2]);
+				System.out.println("== 게시물 수정 ==");
+
+				Article article = getArticle(inp);
+
+				if (article == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", inp);
+					continue;
+				}
 
 			} else if (command.equals("system exit")) {
 				System.out.println("== 프로그램 종료 ==");
