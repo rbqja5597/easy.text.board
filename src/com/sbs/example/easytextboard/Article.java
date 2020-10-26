6efaeb0c5d9013a0efd99020fe7ead40640bb63b
rbqja5597 <rbqja5597@naver.com>
2020-10-27 00:14:01 +0900

    게시물 ArrayList 이용해서 바꾸기

--- src/com/sbs/example/easytextboard/Article.java
+++ src/com/sbs/example/easytextboard/Article.java
@@ -4,4 +4,11 @@ public class Article {
 		public int id;
 		public String title;
 		public String body;
+		
+		Article (int id, String title, String body) {
+			this.id = id;
+			this.title = title;
+			this.body = body;
+			
+		}
 }
